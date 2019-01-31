9e19a9f8558889736952233a8218b6ef0d4c6ab9
hexafluoride <xafluoride@gmail.com>
2019-01-31 14:08:05 +0300

    Initial commit

---
+++ SharpInit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SharpInit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UnitFile.Parse<UnitFile>("./test.unit");
+        }
+    }
+}
---
+++ SharpInit/UnitAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpInit
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class UnitPropertyAttribute : Attribute
+    {
+        public string PropertyPath { get; set; }
+        public UnitPropertyType PropertyType { get; set; }
+
+        public UnitPropertyAttribute(string path, UnitPropertyType type = UnitPropertyType.String)
+        {
+            PropertyPath = path;
+            PropertyType = type;
+        }
+    }
+
+    public enum UnitPropertyType
+    {
+        String,
+        Int,
+        Bool,
+        StringList
+    }
+}
