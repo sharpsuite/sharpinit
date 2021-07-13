93c8a64e829b4f582aabc552746d1972ef5fe155
hexafluoride <xafluoride@gmail.com>
2021-07-13 01:00:21 -0500

    Add implicit deps for mount units

--- SharpInit/Units/MountUnit.cs
+++ SharpInit/Units/MountUnit.cs
@@ -43,6 +43,56 @@ namespace SharpInit.Units
 
         public bool ExternallyActivated { get; set; }
 
+        public override IEnumerable<Dependency> GetImplicitDependencies()
+        {
+            foreach (var dep in base.GetImplicitDependencies())
+                yield return dep;
+            
+            var block_device_path = GetBlockDevicePath();
+
+            if (block_device_path != null)
+            {
+                var escaped_device_name = StringEscaper.EscapePath(block_device_path) + ".device";
+                yield return new RequirementDependency(left: UnitName, right: escaped_device_name, from: UnitName, type: RequirementDependencyType.BindsTo);
+                yield return new OrderingDependency(left: UnitName, right: escaped_device_name, from: UnitName, type: OrderingDependencyType.After);
+            }
+        }
+
+        private string GetBlockDevicePath()
+        {
+            if (string.IsNullOrWhiteSpace(Descriptor.What))
+                return null;
+            
+            var what = Descriptor.What;
+
+            if (what.StartsWith("/dev/"))
+                return what;
+
+            var potential_parts = what.Split('=');
+            if (potential_parts.Length == 1)
+            {
+                return null;
+            }
+
+            var remnants = what.Substring(potential_parts[0].Length + 1);
+
+            switch (potential_parts[1].ToUpperInvariant())
+            {
+                case "PARTUUID":
+                    return $"/dev/disk/by-partuuid/{remnants}";
+                case "UUID":
+                    return $"/dev/disk/by-uuid/{remnants}";
+                case "PARTLABEL":
+                    return $"/dev/disk/by-partlabel/{remnants}";
+                case "LABEL":
+                    return $"/dev/disk/by-label/{remnants}";
+                case "ID":
+                    return $"/dev/disk/by-id/{remnants}";
+                default:
+                    return null;
+            }
+        }
+
         public Transaction GetExternalActivationTransaction(string reason = "Unit externally activated")
         {
             ExternallyActivated = true;
